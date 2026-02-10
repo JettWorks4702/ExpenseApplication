b6e973d6f8cd2d337102ec0ffd82a497febab987
JettWorks4702 <raghavbit6@gmail.com>
2026-02-10 23:28:27 +0530

    First Commit: Added Entities

---
+++ app/src/main/java/org/example/service/JwtService.java
@@ -0,0 +1,38 @@
+package org.example.service;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Base64;
+import java.util.function.Function;
+
+@Service
+public class JwtService {
+
+    public static final String SECRET = "3409580958906098900964509IUREGHU4400";
+
+    public String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimResolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(getSignKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private Key getSignKey() {
+        byte[] keyBytes = Base64.getDecoder().decode(SECRET);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+}
